5c4d9d2a5b8504e522b3b26f6b9cc31ad6d30476
larrylin <u39448@gmail.com>
2013-10-11 17:29:08 +0800

    add2

---
+++ 动画/LeftRightSlide/gen/xc/binny/osdemo/lrslide/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package xc.binny.osdemo.lrslide;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ 动画/LeftRightSlide/src/xc/binny/osdemo/lrslide/SlideSecondActivity.java
@@ -0,0 +1,14 @@
+package xc.binny.osdemo.lrslide;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SlideSecondActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.sec_main);
+	}
+
+}
